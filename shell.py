bf9d0f6b6b81a21bfd9d7cdef81fe23c6064cdac
Jacob Tomlinson <jacob@tom.linson.uk>
2016-07-26 22:57:42 +0100

    Initial commit

---
+++ shell.py
@@ -0,0 +1,35 @@
+import logging
+import os
+import pwd
+import datetime
+from opsdroid.message import Message
+
+class ConnectorShell:
+
+    def __init__(self):
+        """ Init function """
+        self.name = "shell"
+
+    def setup(self, opsdroid):
+        """ Setup the connector """
+        logging.debug("Loaded shell connector")
+
+    def connect(self, opsdroid):
+        """ Connect to the chat service """
+        logging.debug("Connecting to shell")
+        user = pwd.getpwuid(os.getuid())[0]
+        message = ""
+        try:
+            while message != "exit":
+                print('shell> ', end="")
+                user_input = input()
+                message = Message(user_input, user, None, self)
+                opsdroid.parse(message)
+        except (KeyboardInterrupt, EOFError):
+            print('') # Prints a character return to prepare for return to shell
+            logging.info("Keyboard interrupt, exiting shell connector")
+
+    def respond(self, message):
+        """ Respond with a message """
+        logging.debug("Responding with: " + message)
+        print(message)
